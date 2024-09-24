ab9ba37c93faa8d6eac9e9eaa9381710758274f5
yyy96 <yuyifei@sjtu.edu.cn>
2024-09-24 17:54:01 +0800

    add TUAR dataset

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -1,5 +1,5 @@
 from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, PSMSegLoader, \
-    MSLSegLoader, SMAPSegLoader, SMDSegLoader, SWATSegLoader, UEAloader, GLUONTSDataset, EEGDataset
+    MSLSegLoader, SMAPSegLoader, SMDSegLoader, SWATSegLoader, UEAloader, GLUONTSDataset, EEGDataset, TUARDataset
 from data_provider.uea import collate_fn
 import torch
 from torch.utils.data import DataLoader, Subset
@@ -8,6 +8,7 @@ from torch.utils.data.distributed import DistributedSampler
 data_dict = {
     'TUH': EEGDataset,
     'BCICIV': EEGDataset,
+    'TUAR': TUARDataset,
     # 'ETTh1': Dataset_ETT_hour,
     # 'ETTh2': Dataset_ETT_hour,
     # 'ETTm1': Dataset_ETT_minute,
@@ -39,16 +40,13 @@ def data_provider(args, config, flag, ddp=False):  # args,
     if flag == 'test':
         shuffle_flag = False
         drop_last = False
-        if 'anomaly_detection' in config['task_name']:  # working on one gpu
-            batch_size = args.batch_size
-        else:
-            batch_size = 1  # bsz=1 for evaluation
-        freq = args.freq
+        stride_size = config['window_size']
+        batch_size = 1  # bsz=1 for evaluation
     else:
+        stride_size = config['stride_size']
         shuffle_flag = True
         drop_last = True
         batch_size = args.batch_size  # bsz for train and valid
-        freq = args.freq
 
     if 'EEG' in config['task_name']:
 
@@ -56,11 +54,11 @@ def data_provider(args, config, flag, ddp=False):  # args,
             root_path=config['root_path'],
             dataset_name=config['dataset_name'],
             window_size=config['window_size'],
-            stride_size=config['stride_size'],
+            stride_size=stride_size,
+            flag=flag,
             try_run=args.try_run,
         )
 
-        # print("ddp mode is set to false for anomaly_detection", ddp, len(data_set))
         data_loader = DataLoader(
             data_set,
             batch_size=batch_size,
