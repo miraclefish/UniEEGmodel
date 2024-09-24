324d6eccde97ccec18b1775dba078dc3bab1cdb9
yyy96 <yuyifei@sjtu.edu.cn>
2024-09-24 18:01:27 +0800

    model finetune

--- scripts/supervised_learning/UniTS_supervised.sh
+++ scripts/supervised_learning/UniTS_supervised.sh
@@ -1,28 +1,35 @@
-model_name=UniTS
-exp_name=UniTS_supervised_x64
+model_name=UniEEG
+#exp_name=UniTS_supervised_x64
 wandb_mode=online
-project_name=supervised_learning
+project_name=TUAR_sup
 
+d_model=512
 random_port=$((RANDOM % 9000 + 1000))
 
+export WANDB_API_KEY="ad9d2816977a1bcf87acf8a2763df7bc8fdc155a"
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+export OMP_NUM_THREADS=1
+
 # Supervised learning
-torchrun --nnodes 1 --nproc-per-node=1  --master_port $random_port  run.py \
+/home/ps/anaconda3/envs/time_series/bin/torchrun --nnodes 1 --nproc-per-node=4  --master_port $random_port  run.py \
+  --ddp \
+  --try_run \
   --is_training 1 \
-  --model_id $exp_name \
   --model $model_name \
   --lradj supervised \
-  --prompt_num 10 \
-  --patch_len 16 \
-  --stride 16 \
-  --e_layers 3 \
-  --d_model 64 \
+  --prompt_num 8 \
+  --patch_len 256 \
+  --stride 256 \
+  --e_layers 8 \
+  --d_model $d_model \
   --des 'Exp' \
-  --learning_rate 1e-4 \
-  --weight_decay 5e-6 \
-  --train_epochs 5 \
-  --batch_size 32 \
-  --acc_it 32 \
+  --acc_it 1 \
+  --learning_rate 3e-4 \
+  --weight_decay 0 \
+  --prompt_tune_epoch 10 \
+  --train_epochs 10 \
+  --batch_size 256 \
   --debug $wandb_mode \
   --project_name $project_name \
-  --clip_grad 100 \
-  --task_data_config_path data_provider/multi_task.yaml
+  --task_data_config_path  data_provider/multi_task.yaml \
+  --pretrained_weight 'checkpoints/ALL_UniEEG_all_dm512_el8_Exp_07_26_11_53_58/pretrain_checkpoint.pth' > current.log 2>&1 &
