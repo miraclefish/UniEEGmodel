002dc97dca3e6713ea40d29dc624cd344c2f24c0
yyy96 <yuyifei@sjtu.edu.cn>
2024-09-24 18:00:11 +0800

    prepare for model sup ft

--- run.py
+++ run.py
@@ -4,21 +4,46 @@ from exp.exp_sup import Exp_All_Task as Exp_All_Task_SUP
 import random
 import numpy as np
 import wandb
+import logging
+import datetime
+import os
 from utils.ddp import is_main_process, init_distributed_mode
 
+
+def get_logger(log_path):
+    logger = logging.getLogger('Sup')
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # 创建一个handler用于写入日志文件
+    fh = logging.FileHandler(log_path + 'pretrain.log', mode='w')
+    fh.setLevel(logging.INFO)  # 设置handler的日志级别
+    fh.setFormatter(formatter)  # 设置handler的格式
+
+    # 再创建一个handler用于输出到控制台
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)  # 设置handler的日志级别
+    ch.setFormatter(formatter)  # 设置handler的格式
+
+    # 给logger添加handler
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
+    return logger
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='UniTS supervised training')
 
     # basic config
-    parser.add_argument('--task_name', type=str, required=False, default='ALL_task',
+    parser.add_argument('--task_name', type=str, required=False, default='TUAR',
                         help='task name')
-    parser.add_argument('--is_training', type=int,
-                        required=True, default=1, help='status')
-    parser.add_argument('--model_id', type=str, required=True,
+    parser.add_argument('--is_training', type=int, default=1, help='status')
+    parser.add_argument('--model_id', type=str,
                         default='test', help='model id')
-    parser.add_argument('--model', type=str, required=True, default='UniTS',
+    parser.add_argument('--model', type=str, default='UniEEG',
                         help='model name')
+    parser.add_argument('--try_run', action='store_true', help='try run')
 
     # data loader
     parser.add_argument('--data', type=str, required=False,
@@ -30,11 +55,13 @@ if __name__ == '__main__':
     parser.add_argument('--freq', type=str, default='h',
                         help='freq for time features encoding, options:[s:secondly, t:minutely, h:hourly, d:daily, b:business days, w:weekly, m:monthly], you can also use more detailed freq like 15min or 3h')
     parser.add_argument('--task_data_config_path', type=str,
-                        default='exp/all_task.yaml', help='root path of the task and data yaml file')
+                        default='data_provider/multi_task.yaml', help='root path of the task and data yaml file')
     parser.add_argument('--subsample_pct', type=float,
                         default=None, help='subsample percent')
-
+    # device
+    parser.add_argument('--device', type=str, default='cuda:0', help='device')
     # ddp
+    parser.add_argument('--ddp', action='store_true', help='whether to use ddp')
     parser.add_argument('--local-rank', type=int, help='local rank')
     parser.add_argument("--dist_url", default="env://", type=str, help="""url used to set up
         distributed training; see https://pytorch.org/docs/stable/distributed.html""")
@@ -47,7 +74,7 @@ if __name__ == '__main__':
     parser.add_argument('--itr', type=int, default=1, help='experiments times')
     parser.add_argument('--train_epochs', type=int,
                         default=10, help='train epochs')
-    parser.add_argument("--prompt_tune_epoch", type=int, default=0)
+    parser.add_argument("--prompt_tune_epoch", type=int, default=10)
     parser.add_argument('--warmup_epochs', type=int,
                         default=0, help='warmup epochs')
     parser.add_argument('--batch_size', type=int, default=32,
@@ -71,8 +98,9 @@ if __name__ == '__main__':
     parser.add_argument('--dropout', type=float, default=0.1, help='dropout')
     parser.add_argument('--checkpoints', type=str, default='./checkpoints/',
                         help='save location of model checkpoints')
-    parser.add_argument('--pretrained_weight', type=str, default=None,
+    parser.add_argument('--pretrained_weight', type=str, default='/dataYYF/YYF/UniEEGmodel/checkpoints/ALL_UniEEG_all_dm512_el8_Exp_07_26_11_53_58/pretrain_checkpoint.pth',
                         help='location of pretrained model checkpoints')
+    parser.add_argument('--wandb', action='store_true', help='whether to use wandb')
     parser.add_argument('--debug', type=str,
                         default='enabled', help='disabled')
     parser.add_argument('--project_name', type=str,
@@ -82,14 +110,15 @@ if __name__ == '__main__':
     parser.add_argument('--d_model', type=int, default=512,
                         help='dimension of model')
     parser.add_argument('--n_heads', type=int, default=8, help='num of heads')
-    parser.add_argument('--e_layers', type=int, default=2,
+    parser.add_argument('--e_layers', type=int, default=8,
                         help='num of encoder layers')
     parser.add_argument("--share_embedding",
                         action="store_true", default=False)
-    parser.add_argument("--patch_len", type=int, default=16)
-    parser.add_argument("--stride", type=int, default=8)
-    parser.add_argument("--prompt_num", type=int, default=5)
+    parser.add_argument("--patch_len", type=int, default=256)
+    parser.add_argument("--stride", type=int, default=256)
+    parser.add_argument("--prompt_num", type=int, default=8)
     parser.add_argument('--fix_seed', type=int, default=None, help='seed')
+    parser.add_argument('--mode_debug', type=bool, default=False, help='whether to debug')
 
     # task related settings
     # forecasting task
@@ -111,56 +140,53 @@ if __name__ == '__main__':
                         type=str, default=None, help='unify')
 
     args = parser.parse_args()
-    init_distributed_mode(args)
+    if args.ddp:
+        init_distributed_mode(args)
     if args.fix_seed is not None:
         random.seed(args.fix_seed)
         torch.manual_seed(args.fix_seed)
         np.random.seed(args.fix_seed)
 
-    print('Args in experiment:')
-    print(args)
-    exp_name = '{}_{}_{}_{}_ft{}_dm{}_el{}_{}'.format(
-        args.task_name,
-        args.model_id,
-        args.model,
-        args.data,
-        args.features,
-        args.d_model,
-        args.e_layers,
-        args.des)
-
-    if int(args.prompt_tune_epoch) != 0:
-        exp_name = 'Ptune'+str(args.prompt_tune_epoch)+'_'+exp_name
-        print(exp_name)
+    date_str = datetime.datetime.now().strftime('%m_%d_%H_%M_%S')
+    exp_name = f'{args.task_name}_{args.model}_{args.data}_' \
+               f'dm{args.d_model}_el{args.e_layers}_{args.des}_{date_str}'
+    log_path = args.checkpoints + 'supervise/' + exp_name + '/'
 
     if is_main_process():
-        wandb.init(
-            name=exp_name,
-            # set the wandb project where this run will be logged
-            project=args.project_name,
-            # track hyperparameters and run metadata
-            config=args,
-            mode=args.debug,
-        )
+        if not os.path.exists(log_path):
+            os.makedirs(log_path)
+
+        if args.wandb:
+            wandb.init(
+                name=exp_name,
+                # set the wandb project where this run will be logged
+                project=args.project_name,
+                # track hyperparameters and run metadata
+                config=args,
+                mode=args.debug,
+            )
+
+        logger = get_logger(log_path)
+        logger.info('Args in experiment:')
+        logger.info(args)
+
+        if int(args.prompt_tune_epoch) != 0:
+            exp_name = 'Tune'+str(args.prompt_tune_epoch)+'_'+exp_name
+            logger.info(exp_name)
+    else:
+        logger = None
 
     Exp = Exp_All_Task_SUP
 
     if args.is_training:
-        for ii in range(args.itr):
-            # setting record of experiments
-            setting = '{}_{}_{}_{}_ft{}_dm{}_el{}_{}_{}'.format(
-                args.task_name,
-                args.model_id,
-                args.model,
-                args.data,
-                args.features,
-                args.d_model,
-                args.e_layers,
-                args.des, ii)
+        # setting record of experiments
+        setting = exp_name
 
-            exp = Exp(args)  # set experiments
-            print('>>>>>>>start training : {}>>>>>>>>>>>>>>>>>>>>>>>>>>'.format(setting))
-            exp.train(setting)
+        exp = Exp(args, logger)  # set experiments
+        if logger:
+            logger.info('>>>>>>>start training : {}>>>>>>>>>>>>>>>>>>>>>>>>>>'.format(setting))
+        exp.train(setting)
+        torch.cuda.empty_cache()
     else:
         ii = 0
         setting = '{}_{}_{}_{}_ft{}_dm{}_el{}_{}_{}'.format(
@@ -176,4 +202,4 @@ if __name__ == '__main__':
         exp = Exp(args)  # set experiments
         print('>>>>>>>testing : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
         exp.test(setting, load_pretrain=True)
-        torch.cuda.empty_cache()
+
